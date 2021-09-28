f5ca234ba3f351bbfb498b52f8c7a4c45a28f95e
Anas Raza <anus.raza987@gmail.com>
2021-09-28 23:06:14 +0500

    Initial commit

---
+++ youtube/prc.js
@@ -0,0 +1,35 @@
+
+// var person = { fname: "John", lname: "Doe", age: 25 }
+
+// for (let x in person) {
+//   console.log(person);
+// }
+
+// var txt = 'JavaScript';
+
+// for (let x of txt) {
+//   console.log(txt[x])
+// }
+
+// for (let i = 0; i < 10; i++) {
+//   for (let j = 0; j <= i; j++) {
+//     document.write("*")
+//   }
+//   document.write(" <br/>")
+// }
+
+// for (let i = 1; i < 11; i++) {
+//   document.write(`2 * ${i} = ${2 * i} <br/>`);
+// }
+
+// let input = prompt("Enter num you want to print table");
+// let quan = prompt("Enter num you want to print table");
+
+// function printTable(inp) {
+//   for (let i = 1; i <= quan; i++) {
+//     document.write(`${inp} * ${i} = ${inp * i} <br/>`);
+//   }
+// }
+
+// printTable(input);
+
