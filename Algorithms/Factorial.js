9fe759677464d3882223668c3ad4dbb39239aa2a
Anas Raza <anus.raza987@gmail.com>
2022-10-14 00:20:01 +0500

    Factorial of a number

---
+++ Algorithms/Factorial.js
@@ -0,0 +1,19 @@
+// Factorial of a non-negative integer is the product of all positive integers less than or equal to 'n'.
+
+function factorial(n) {
+  let result = 1;
+  for (let i = 2; i <= n; i++) {
+    result = result * i;
+  }
+  return result;
+}
+
+console.log(factorial(0)); // 1
+console.log(factorial(1)); // 1
+console.log(factorial(2)); // 2
+console.log(factorial(3)); // 6
+console.log(factorial(4)); // 24
+console.log(factorial(5)); // 120
+
+// Time Complexity
+// Big-O = O(n) Linear
